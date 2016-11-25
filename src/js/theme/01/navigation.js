ad381d037c9a6d95d5706f239b72ae3533ab7f02
Heather Weaver <heather@hweaver.com>
2016-11-25 17:23:20 -0600

    Added animation to mobile navigation trigger

--- src/js/theme/01/navigation.js
+++ src/js/theme/01/navigation.js
@@ -6,7 +6,7 @@ var mobileNav = function() {
             $('.navigation--mobile__container').css('display', 'block');
             //$('nav li.logo').css('display', 'none');
             $('.navigation--mobile__page').animate({ right:250 }, '200', function() {
-                $('.navigation--mobile__trigger i').removeClass('fa-navicon').addClass('fa-times');
+                $('.navigation--mobile__trigger').addClass('navigation--mobile__trigger--arrow');
             });
             //$('.navigation--primary').css('position', 'absolute').animate({right:250}, 'slow');
         }
@@ -14,7 +14,7 @@ var mobileNav = function() {
             $('.navigation--mobile__page').animate({ right:0 }, '200', function() {
                 $('.navigation--mobile__page').css('position', 'absolute');
             	//$('nav li.logo').css('display', 'block');
-                $('.navigation--mobile__trigger i').removeClass('fa-times').addClass('fa-navicon');
+                $('.navigation--mobile__trigger').removeClass('navigation--mobile__trigger--arrow');
                 $('.navigation--mobile__container').css('display', 'none');
 
             });
--- templates/top-navigation.php
+++ templates/top-navigation.php
@@ -5,7 +5,7 @@
 				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/dummy-logo.png" class="navigation--primary__logo" alt="<?php echo get_option('blogname'); ?> Logo">
 			</a>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu', 'menu_class' => 'navigation--primary__menu list--inline list--unstyled', 'container' => '' ) ); ?>
-			<a href="#" id="mobile-menu-trigger" class="navigation--mobile__trigger" title="Open Mobile Navigation"><i class="fa fa-navicon"></i></a>
+			<a href="#" id="mobile-menu-trigger" class="navigation--mobile__trigger" title="Open Mobile Navigation"><span>Toggle Menu</span></a>
 		</div>
 	</div>
 	<div class="navigation--primary__container--fixed">
