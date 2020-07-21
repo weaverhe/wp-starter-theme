131d9f9143dd3044a6d757240d12175100da7dda
Heather Weaver <heather@hweaver.com>
2020-07-21 17:45:25 -0500

    Added static asset loader examples

---
+++ app/assets/js-assets.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Load static JS files
+ *
+ * @package wp-starter
+ * @author Heather Weaver
+ * @version 1.0
+ * @since 1.0
+ */
+
+/**
+ * Example:
+ * 
+ * $default_url = _templateURL . '/assets/js';
+ * load_js( $default_url . '/application.js' );
+ */
